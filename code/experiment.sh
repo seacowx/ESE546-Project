1835060424c5d18a841fb8b31505b62bb97f106a
SeacowX <jxu9716@gmail.com>
2022-12-18 19:52:32 -0500

    add experiment files

---
+++ code/experiment.sh
@@ -0,0 +1,3 @@
+python train.py --dataset snli --content all --epochs 10 --model bert --mix_frac 0.3
+python train.py --dataset snli --content all --epochs 10 --model bert --mix_frac 0.5
+python train.py --dataset snli --content all --epochs 10 --model bert --mix_frac 0.7
